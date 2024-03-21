ac7ef164bdb7e4135a58b7a19f2ec7393d769cd5
Axhu99 <xhuliodreni@icloud.com>
2024-03-21 17:42:09 +0100

    fix factory

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
  */
-class PostFactory extends Factory
+class ProjectFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -19,7 +19,7 @@ class PostFactory extends Factory
     {
         $title = fake()->text(20);
         return [
-            'title' => fake()->text(20),
+            'title' => $title,
             'slug' => Str::slug($title),
             'content' => fake()->paragraph(15, true),
             'image' => fake()->imageUrl(250, 250, true),
